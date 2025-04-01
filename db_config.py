47b8632e3757dd8cb1ea85da19c6749ba70d1470
Sharleen Liz <lizsharleen381@gmail.com>
2025-04-02 01:44:15 +0300

    my project

---
+++ db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+from urllib.parse import quote_plus
+DB_USER = "MySQL80"
+DB_PASSWORD="Lovemeshally%402004"
+DB_HOST = "localhost"
+DB_PORT = "3306"
+DB_NAME = "inflationdb"
+DB_URL = f"mysql+mysqlconnector://root:Lovemeshally%402004@localhost:3306/inflationdb"
